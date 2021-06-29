13c7448555d0ea9e0adc0bf53d172b8e97d59801
Manish Khanna <45462873+manishkhanna3002@users.noreply.github.com>
2021-06-29 21:20:19 +0530

    Add files via upload

--- Untitled-7.js
+++ Untitled-7.js
@@ -1,4 +1,4 @@
-class person
+module.exports = class person
 {
     //The following properties like age, location are class properties
     age = 24
@@ -24,11 +24,11 @@ fullName()
 
 }
 
-let personobj = new person("Tim","Joseph")
+/*let personobj = new person("Tim","Joseph")
 let personobj1 = new person("Chris","Jones")
 personobj.fullName()
 personobj1.fullName()
 console.log(personobj.age)
-console.log(personobj.location) //You don't use () brackets at the end of location because when you use get then it is a property not a method/function
+console.log(personobj.location) //You don't use () brackets at the end of location because when you use get then it is a property not a method/function*/
 
 //Constructor is a method which excutes by default when you create an object of the class
